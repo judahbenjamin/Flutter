ba75df4257408ae9286fb9f2ea5ce7e7918e9fdd
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2025-04-29 08:59:58 -0300

    Update lista_imagens.dart

--- lista_imagens.dart
+++ lista_imagens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:typed_data';
 
 void main() {
   runApp(MyApp());
@@ -24,24 +25,127 @@ class ListaDeImagensScreen extends StatefulWidget {
 }
 
 class _ListaDeImagensScreenState extends State<ListaDeImagensScreen> {
-  List<String> _selectedImagePaths = [];
+  List<Uint8List?> _selectedImageBytes = [];
+  List<String> _selectedImageNames = [];
+  List<Uint8List?> _storedImageBytes = [];
+  List<String> _storedImageNames = [];
+  final int _maxFileSizeMB = 5; // Define o tamanho máximo do arquivo em MB
 
   Future<void> _selectImages() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.image, // Filtra para mostrar apenas arquivos de imagem
-      allowMultiple: true, // Permite selecionar múltiplos arquivos
+      type: FileType.image,
+      allowMultiple: true,
     );
 
     if (result != null && result.files.isNotEmpty) {
-      setState(() {
-        _selectedImagePaths = result.paths!.whereType<String>().toList();
-        // Aqui você pode adicionar a lógica para exibir os nomes dos arquivos
-        // ou as próprias imagens na área superior, se desejar.
-        print('Caminhos das imagens selecionadas: $_selectedImagePaths');
-      });
+      List<PlatformFile> files = result.files;
+      List<Uint8List?> newImageBytes = [];
+      List<String> newImageNames = [];
+      bool hasDuplicates = false;
+
+      // Verificação de nomes duplicados e tamanho dos arquivos
+      for (PlatformFile file in files) {
+        if (_selectedImageNames.contains(file.name) || _storedImageNames.contains(file.name)) {
+          hasDuplicates = true;
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Erro: O arquivo "${file.name}" já foi selecionado ou armazenado.')),
+          );
+        } else if (file.size > _maxFileSizeMB * 1024 * 1024) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Erro: O arquivo "${file.name}" excede o tamanho máximo de ${_maxFileSizeMB}MB.')),
+          );
+        } else {
+          newImageBytes.add(file.bytes);
+          newImageNames.add(file.name);
+        }
+      }
+
+      if (!hasDuplicates && newImageNames.isNotEmpty) {
+        setState(() {
+          _selectedImageBytes.addAll(newImageBytes);
+          _selectedImageNames.addAll(newImageNames);
+          print('Bytes das imagens selecionadas: $_selectedImageBytes');
+          print('Nomes das imagens selecionadas: $_selectedImageNames');
+        });
+      }
     } else {
-      // O usuário cancelou a seleção ou não escolheu nenhum arquivo.
       print('Nenhuma imagem selecionada.');
+    }
+  }
+
+  void _gravarImagens() {
+    if (_selectedImageNames.isNotEmpty && _selectedImageBytes.isNotEmpty) {
+      setState(() {
+        _storedImageBytes.addAll(_selectedImageBytes);
+        _storedImageNames.addAll(_selectedImageNames);
+        _selectedImageBytes.clear();
+        _selectedImageNames.clear();
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('${_storedImageNames.length} imagens foram armazenadas.')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Nenhuma imagem selecionada para gravar.')),
+      );
+    }
+  }
+
+  Widget _buildImageSelectionArea() {
+    if (_selectedImageNames.isNotEmpty) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Imagens selecionadas:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 4.0,
+              children: _selectedImageNames.map((name) => Chip(label: Text(name))).toList(),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return GestureDetector(
+        onTap: _selectImages,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.arrow_right),
+              SizedBox(width: 8.0),
+              Text('Seleciona arquivo para envio'),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
+  void _mostrarImagem(BuildContext context, Uint8List? imageBytes, String imageName) {
+    if (imageBytes != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ExibeImagemScreen(imageBytes: imageBytes, imageName: imageName),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar a imagem.')),
+      );
     }
   }
 
@@ -49,7 +153,7 @@ class _ListaDeImagensScreenState extends State<ListaDeImagensScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de imagens armazenadas'),
+        title: Text('Meu App de Imagens'),
       ),
       body: Column(
         children: <Widget>[
@@ -58,50 +162,86 @@ class _ListaDeImagensScreenState extends State<ListaDeImagensScreen> {
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.black),
               ),
-              // Aqui você exibirá as imagens selecionadas ou os nomes dos arquivos
-              // Por enquanto, deixamos vazio.
+              child: _storedImageNames.isEmpty
+                  ? Center(
+                      child: Text('Nenhuma imagem armazenada ainda.'),
+                    )
+                  : ListView.builder(
+                      itemCount: _storedImageNames.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            _mostrarImagem(context, _storedImageBytes[index], _storedImageNames[index]);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('${index + 1}. ${_storedImageNames[index]}'),
+                          ),
+                        );
+                      },
+                    ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: _selectImages, // Chama a função ao tocar
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.arrow_right),
-                      SizedBox(width: 8.0),
-                      Text('Seleciona arquivo para envio'),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            child: _buildImageSelectionArea(),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {
-                  // Aqui você implementará a lógica para gravar/armazenar as imagens
-                  if (_selectedImagePaths.isNotEmpty) {
-                    print('Imagens prontas para serem gravadas: $_selectedImagePaths');
-                    // Implemente a lógica de armazenamento aqui.
-                  } else {
-                    print('Nenhuma imagem selecionada para gravar.');
-                  }
-                },
+                onPressed: _gravarImagens,
                 child: Text('GRAVAR'),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ExibeImagemScreen extends StatelessWidget {
+  final Uint8List imageBytes;
+  final String imageName;
+
+  ExibeImagemScreen({required this.imageBytes, required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Imagem Selecionada'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.memory(imageBytes),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                imageName,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('VOLTAR'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
